8ddb885ebaafd05a5b1cc4998b243a1fd35e254f
test13234 <lakshwin045@gmail.com>
2021-04-04 20:44:06 +0530

    change post date + fix typos

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Towards Extreme Generalization",
-        "excerpt":"In his paper titled, “On the Measure of Intelligence”, François Chollet provides a definition of the trait that serves as a metric for future ML research. He describes intelligence as that which enables wild generalization across tasks, optimized specifically for flexibility and adaptability. Unfortunately trends in ML research today are...","categories": [],
+        "excerpt":"In 2019, François Chollet provided one of the most comprehensive definitions of intelligence. He described intelligence as that which enables wild generalization across tasks, optimized specifically for flexibility and adaptability. (Chollet, 2019) Unfortunately trends in ML research today are a far cry from his description, leveraging unlimited priors to showcase...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/Towards-Extreme_Generalization/",
         "teaser": null
