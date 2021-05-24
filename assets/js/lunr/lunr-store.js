e594e4a00f7f5b8fac9b393ac000330d483a7391
Lakshwin Shreesha <lakshwin045@gmail.com>
2021-05-24 15:05:56 +0530

    supressed about page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Towards Extreme Generalization",
         "excerpt":"In 2019, François Chollet provided one of the most comprehensive definitions of intelligence to date. He described the trait as that which enables wild generalization across tasks, optimized specifically for flexibility and adaptability. [Chollet 2019] Unfortunately trends in ML research today are a far cry from his description, leveraging unlimited...","categories": [],
         "tags": [],
-        "url": "/Towards-Extreme_Generalization/",
+        "url": "https://niwhskal.github.io/Towards-Extreme_Generalization/",
         "teaser": null
       }]
