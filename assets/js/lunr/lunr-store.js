2e00137d33565ea9b974428ec92779c42656f6ce
Lakshwin Shreesha <lakshwin045@gmail.com>
2021-06-05 16:47:41 +0530

    new post--Computability of Life--5th June 2021

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Towards Extreme Generalization",
-        "excerpt":"In 2019, François Chollet provided one of the most comprehensive definitions of intelligence to date. He described the trait as that which enables wild generalization across tasks, optimized specifically for flexibility and adaptability. [Chollet 2019] Unfortunately trends in ML research today are a far cry from his description, leveraging unlimited...","categories": [],
+        "excerpt":"In 2019, François Chollet provided one of the most comprehensive definitions of intelligence to date. He described the trait as that which enables wild generalization across tasks, optimized specifically for flexibility and adaptability. (Chollet, 2019) Unfortunately trends in ML research today are a far cry from his description, leveraging unlimited...","categories": [],
         "tags": [],
         "url": "https://niwhskal.github.io/Towards-Extreme_Generalization/",
+        "teaser": null
+      },{
+        "title": "Computability of Life",
+        "excerpt":"When Alan Turing laid the principles of computing, he presupposed that it’s capabilities be limitless. A machine which could carry out any computation no matter the complexity— Turing complete was what he called it. Rightly so, now, in 2021, we have the modern computer which embodies his vision — an...","categories": [],
+        "tags": [],
+        "url": "https://niwhskal.github.io/Computability-Of-Life/",
         "teaser": null
       }]
